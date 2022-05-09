e61aac1653ae5ffe2265a8e1aa6cdfcb08da0cf1
rajdevi14 <rajdevi14@gmail.com>
2022-05-09 22:47:22 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,8 @@
+const add=(n1,n2)=>{
+    return n1+n2;}
+
+const sub=(n1,n2)=>
+    {
+        return n1-n2;
+    }
+module.exports={add}
---
+++ server.js
@@ -0,0 +1,26 @@
+const {add}=require('./calculator')
+
+//const { add, sub, mul, div } = require('./arithmetic.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { urlencoded } = require('express');
+const app = express()
+
+//app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(cors())
+
+
+app.post('/add', (req, res) => {
+    const a = req.body.a;
+    const b = req.body.b;
+    const result = add(a, b);
+    res.json({ result });
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log("Server Started On PORT : " + PORT);
+})
